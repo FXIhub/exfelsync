50212136066f42e2994628543fc2efefa5bc6ee7
Benedikt Daurer <benedikt.daurer@icm.uu.se>
2017-11-29 23:21:54 +0100

    Added dealer

---
+++ dealer.py
@@ -0,0 +1,17 @@
+import time, sys
+
+dealer = lambda *args: Dealer(*args).start()
+class Dealer(object):
+    def __init__(self, socket, buf):
+        self._socket = socket
+        # TODO: Create a ZMQ service that can deal out data to a client
+        self._buf = buf
+
+    def start(self):
+        print("Starting reading...")
+        while(True):
+            print("Reader", len(self._buf))
+            sys.stdout.flush()
+            time.sleep(1)
+
+        
